36a21628018164777cf1dcf6ab6583300d2cac08
viinay <viinay@users.noreply.github.com>
2018-03-01 00:18:38 +0530

    added sync read function

--- readfile.js
+++ readfile.js
@@ -1,5 +1,5 @@
 var fs  = require("fs");
-
+#readFile with utf-8 format
 fs.readFile("file2.txt","utf-8",function(err,data){
 	if(err){
 		console.error(err);
@@ -7,4 +7,9 @@ fs.readFile("file2.txt","utf-8",function(err,data){
 		console.log(data);
 	}
 })
+console.log(".end");
+
+#readFile Syncly with utf-8 format
+var data = fs.readFileSync("file2.txt","utf-8");
+console.log(data);
 console.log(".end");
